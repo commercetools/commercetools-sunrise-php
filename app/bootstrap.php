1043d6b30d0f7ab2f6709152c951c75cdc5a1ca9
Jens Schulze <jens.schulze@commercetools.de>
2015-09-04 10:32:36 +0200

    add heroku config

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -13,6 +13,7 @@ use Commercetools\Sunrise\Template\TemplateService;
 use Silex\Application;
 use Commercetools\Core\Model\Common\Context;
 use Silex\Provider\LocaleServiceProvider;
+use Silex\Provider\MonologServiceProvider;
 use Silex\Provider\TranslationServiceProvider;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -22,14 +23,23 @@ use Symfony\Component\Translation\Translator;
 require __DIR__.'/../vendor/autoload.php';
 
 define('PROJECT_DIR', dirname(__DIR__));
-const DEFAULT_LANGUAGE = 'de';
+const DEFAULT_LOCALE = 'de-de';
+const LOCALE_PATTERN = '[a-z]{2}-[a-z]{2}';
 
 $app = new Application();
 $app->register(new LocaleServiceProvider());
 
+// Register the monolog logging service
+$app->register(new MonologServiceProvider(), array(
+    'monolog.logfile' => 'php://stderr',
+));
+
+$app['locale'] = DEFAULT_LOCALE;
+
 $app['client'] = function () use ($app) {
-    $language = $app['languages'][$app['locale']];
-    $context = Context::of()->setLanguages($language)->setGraceful(true);
+    $language = \Locale::getPrimaryLanguage($app['locale']);
+    $languages = $app['languages'][$language];
+    $context = Context::of()->setLanguages($languages)->setGraceful(true)->setLocale('de')->setLocale($app['locale']);
     if (file_exists(__DIR__ .'/myapp.ini')) {
         $appConfig = parse_ini_file(__DIR__ .'/myapp.ini', true);
         $config = $appConfig['commercetools'];
@@ -47,10 +57,7 @@ $app['client'] = function () use ($app) {
 $app['cache'] = function () use ($app) {
     $factory = new CacheAdapterFactory();
 
-    $redis = new \Redis();
-    $redis->connect('localhost');
-
-    return $factory->get($redis);
+    return $factory->get();
 };
 
 $app->register(new TranslationServiceProvider(), ['translator.cache_dir' => PROJECT_DIR . '/cache/translation']);
@@ -68,42 +75,40 @@ $app['view'] = function () {
     return new TemplateService(new HandlebarsAdapter(PROJECT_DIR .'/cache/templates'));
 };
 
-$app->get('/', 'Commercetools\Sunrise\Controller\CatalogController::home')->value('lang', DEFAULT_LANGUAGE);
+$app->get('/', 'Commercetools\Sunrise\Controller\CatalogController::home')->value('lang', DEFAULT_LOCALE);
 
 $app->get('/{_locale}', 'Commercetools\Sunrise\Controller\CatalogController::home')
-    ->assert('_locale', 'en|de')
+    ->assert('_locale', LOCALE_PATTERN)
     ->bind('home');
 $app->get('/{_locale}/search', 'Commercetools\Sunrise\Controller\CatalogController::search')
-    ->assert('_locale', 'en|de')
+    ->assert('_locale', LOCALE_PATTERN)
     ->bind('pop');
 $app->get('/{_locale}/{slug}.html', 'Commercetools\Sunrise\Controller\CatalogController::detail')
-    ->assert('_locale', 'en|de')
+    ->assert('_locale', LOCALE_PATTERN)
     ->bind('pdp');
 $app->get('/{_locale}/{category1}', 'Commercetools\Sunrise\Controller\CatalogController::search')
-    ->assert('_locale', 'en|de');
-$app->get('/{_locale}/{category1}/{category2}', 'Commercetools\Sunrise\Controller\CatalogController::search')
-    ->assert('_locale', 'en|de');
+    ->assert('_locale', LOCALE_PATTERN);
 
 // location redirects for trailing slashes
 $app->get('/{_locale}/', function(Application $app) {
     return $app->redirect('/'.$app['locale']);
-})->assert('_locale', 'en|de');
+})->assert('_locale', LOCALE_PATTERN);
 $app->get('/{_locale}/search/', function($category1, $category2, Application $app) {
     return $app->redirect('/'. $app['locale'] . '/search');
-})->assert('_locale', 'en|de');
+})->assert('_locale', LOCALE_PATTERN);
 $app->get('/{_locale}/{category1}/', function($category1, Application $app) {
     return $app->redirect('/'. $app['locale'] . '/' . $category1);
-})->assert('_locale', 'en|de');
+})->assert('_locale', LOCALE_PATTERN);
 
 // location redirects for uri without locale information
 $app->get('/search', function(Application $app) {
-    return $app->redirect('/'.DEFAULT_LANGUAGE.'/search');
+    return $app->redirect('/'.DEFAULT_LOCALE.'/search');
 });
 $app->get('/{slug}.html', function(Application $app, $slug) {
-    return $app->redirect('/'.DEFAULT_LANGUAGE.'/' . $slug . '.html');
+    return $app->redirect('/'.DEFAULT_LOCALE.'/' . $slug . '.html');
 });
 $app->get('/{category1}', function(Application $app, $category1) {
-    return $app->redirect('/' . DEFAULT_LANGUAGE . '/' . $category1);
+    return $app->redirect('/' . DEFAULT_LOCALE . '/' . $category1);
 });
 
 $app->error(function (NotFoundHttpException $e) use ($app) {
