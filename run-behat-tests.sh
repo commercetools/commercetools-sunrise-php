081d1678a0b807e211d47d84fcbcdef0699bd0fb
Jens Schulze <jens.schulze@commercetools.de>
2015-09-17 14:52:34 +0200

    typo

--- run-behat-tests.sh
+++ run-behat-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export BEHAT_PARAMS="{\"extensions\": { \"Behat\\MinkExtension\": {\"sessions\": {\"sauce\": {\"selenium2\": {\"wd_host\":\"${SAUCE_USERNAME}:${SAUCE_ACCESS_KEY}@ondemand.saucelabs.com/wd/hub\"}}}}}}"
+export BEHAT_PARAMS="{\"extensions\": { \"Behat\\\\MinkExtension\": {\"sessions\": {\"sauce\": {\"selenium2\": {\"wd_host\":\"${SAUCE_USERNAME}:${SAUCE_ACCESS_KEY}@ondemand.saucelabs.com/wd/hub\"}}}}}}"
 
 vendor/bin/behat -p sunrise
 vendor/bin/behat
