ed5963f876a316d7680c907a23343d30e8b9f49e
Jens Schulze <jens.schulze@commercetools.de>
2015-09-17 15:00:30 +0200

    fix binding for local client

--- run-behat-tests.sh
+++ run-behat-tests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-export BEHAT_PARAMS="{\"extensions\": { \"Behat\\\\MinkExtension\": {\"sessions\": {\"sauce\": {\"selenium2\": {\"wd_host\":\"${SAUCE_USERNAME}:${SAUCE_ACCESS_KEY}@ondemand.saucelabs.com/wd/hub\"}}}}}}"
+export BEHAT_PARAMS="{\"extensions\": { \"Behat\\\\MinkExtension\": {\"sessions\": {\"sauce\": {\"selenium2\": {\"wd_host\":\"${SAUCE_USERNAME}:${SAUCE_ACCESS_KEY}@ondemand.saucelabs.com/wd/hub\", \"capabilities\": {\"name\": \"Sunrise Live\"}}}}}}}"
 
 vendor/bin/behat -p sunrise
+
+export BEHAT_PARAMS="{\"extensions\": { \"Behat\\\\MinkExtension\": {\"sessions\": {\"sauce\": {\"selenium2\": {\"wd_host\":\"${SAUCE_USERNAME}:${SAUCE_ACCESS_KEY}@ondemand.saucelabs.com/wd/hub\", \"capabilities\": {\"name\": \"Sunrise PHP\"}}}}}}}"
+
 vendor/bin/behat
