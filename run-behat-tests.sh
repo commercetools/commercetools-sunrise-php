dbca6b9ff14d49beb64fc9bfe130b4ea39ba6ca7
Jens Schulze <jens.schulze@commercetools.de>
2015-09-17 14:49:59 +0200

    fix travis config for saucelabs

---
+++ run-behat-tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export BEHAT_PARAMS="{\"extensions\": { \"Behat\\MinkExtension\": {\"sessions\": {\"sauce\": {\"selenium2\": {\"wd_host\":\"${SAUCE_USER}:${SAUCE_ACCESS_KEY}@ondemand.saucelabs.com/wd/hub\"}}}}}}"
+
+vendor/bin/behat -p sunrise
+vendor/bin/behat
--- setup-tests.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-export BEHAT_PARAMS='{"extensions": { "Behat\\MinkExtension": {"sessions": {"sauce": {"selenium2": {"wd_host":"'$SAUCE_USER':'$SAUCE_ACCESS_KEY'@ondemand.saucelabs.com/wd/hub"}}}}}}'
