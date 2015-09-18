693e87f21e8529f1e10243e1ca33eb39244cd6c0
Jens Schulze <jens.schulze@commercetools.de>
2015-09-18 12:05:22 +0200

    typo

--- run-behat-tests.sh
+++ run-behat-tests.sh
@@ -4,6 +4,6 @@ PROFILES=${BEHAT_PROFILES:-""}
 
 export BEHAT_PARAMS='{"extensions": { "Behat\\MinkExtension": {"sessions": {"sauce": {"selenium2": {"wd_host":"${SAUCE_USERNAME}:${SAUCE_ACCESS_KEY}@ondemand.saucelabs.com/wd/hub"}}}}}}'
 
-for PROFILES IN $PROFILES do
+for PROFILE in $PROFILES; do
     vendor/bin/behat -p $PROFILE
 done
