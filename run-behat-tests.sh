20583abdd5ba51c4d1e20dd7c7a88208319fe956
Jens Schulze <jens.schulze@commercetools.de>
2015-09-21 18:36:20 +0200

    simplify behat test running

--- run-behat-tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export BEHAT_PARAMS='{"extensions": { "Behat\\MinkExtension": {"sessions": {"sauce": {"selenium2": {"wd_host":"'${SAUCE_USERNAME}':'${SAUCE_ACCESS_KEY}'@ondemand.saucelabs.com/wd/hub"}}}}}}'
-
-echo "Running profile: "$BEHAT_PROFILE
-vendor/bin/behat --profile $BEHAT_PROFILE
---
+++ tests/behat/SunriseContext.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Commercetools\Sunrise;
+
+use Behat\Behat\Context\SnippetAcceptingContext;
+
+/**
+ * @author @ct-jensschulze <jens.schulze@commercetools.de>
+ */
+class SunriseContext extends BehatContext implements SnippetAcceptingContext
+{
+}
