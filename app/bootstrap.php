3d080ed831dc4cbf1df7b37dd24137fae5a1b84e
Jens Schulze <jens.schulze@commercetools.de>
2016-04-08 11:23:06 +0200

    fix(App): disable debug mode for production environment

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -12,7 +12,7 @@ define('PROJECT_DIR', dirname(__DIR__));
 require __DIR__.'/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
 
-$kernel = new \AppKernel('prod', true);
+$kernel = new \AppKernel('prod', false);
 $kernel->loadClassCache();
 $kernel = new \AppCache($kernel);
 
